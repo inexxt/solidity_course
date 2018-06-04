21f44593f4c615857a9d330f6dae9c31a4c48836
jack <jac.karwowski@gmail.com>
2018-06-04 09:29:26 +0200

    More integration tests, everything working both locally and on rinkeby

--- project/app/test/test_integration.py
+++ project/app/test/test_integration.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import List
 
+import time
 from web3.testing import Testing
 
 from backend.Server import Server
@@ -65,29 +66,83 @@ class TestIntegration(unittest.TestCase):
             r = f[e].buy(price, channel_number=c)
             self.assertTrue(b.approveBuy(item, self.receipt_convert(r)))
             f_spent[e]+= price
+            return c
 
-        create_buy(0, "esher")
-        create_buy(0, "monalisa")
+        c00 = create_buy(0, "esher")
+        c01 = create_buy(0, "monalisa")
         self.assertRaises(Exception, create_buy, 0, "monalisa", 1)
 
-        create_buy(1, "space")
-        create_buy(2, "space")
+        c10 = create_buy(1, "space")
+        c20 = create_buy(2, "space")
 
-        f[2].st.startClosingChannel(0, f[2].st.curr_used_funds[0])
+        f[2].st.startClosingChannel(c20, f[2].st.curr_used_funds[0])
         self.waitWaitingPeriod()
-        f[2].st.closeChannel(0)
+        f[2].st.closeChannel(c20)
 
-        self.assertRaises(Exception, create_buy, 2, "esher", 0)
+        self.assertRaises(Exception, create_buy, 2, "esher", c20)
 
-        b.st.closeChannel(f[0].st.account, 0)
-        b.st.closeChannel(f[0].st.account, 1)
-        b.st.closeChannel(f[1].st.account, 0)
+        b.st.closeChannel(f[0].st.account, c00)
+        b.st.closeChannel(f[0].st.account, c01)
+        b.st.closeChannel(f[1].st.account, c10)
 
-        self.assertRaises(Exception, create_buy, 0, "esher", 0)
+        self.assertRaises(Exception, create_buy, 0, "esher", c10)
 
         self.assertAlmostEqual(self.starting_balance_back / (self.getBackBalance() - w(sum(f_spent))), 1)
         for e in range(len(f)):
             self.assertAlmostEqual(self.starting_balance_front[e] / (self.getFrontBalance(e) + w(f_spent[e])), 1)
+
+    def test_dishonest_user_prevention(self):
+        Id = 5
+        f: Client = self.fronts[Id]
+        b: Server = self.back
+
+        w = W3cls.ethToWei
+
+        content = b.content
+
+        def create_buy(item, c=None):
+            if c is None:
+                c = f.st.createNewChannel(w(25))
+            price = content[item]["price"]
+            r = f.buy(price, channel_number=c)
+            self.assertTrue(b.approveBuy(item, self.receipt_convert(r)))
+            return c, price
+
+        c, spent = create_buy("monalisa")
+        assert w(3) < w(spent)
+
+        f.st.startClosingChannel(c, w(3))
+        b.performMaintenanceUserAccounts()
+
+        pun = b.st.contract.functions.PUNISHMENT().call()
+        self.assertAlmostEqual(self.starting_balance_back / (self.getBackBalance() - w(spent) - pun), 1)
+        self.assertAlmostEqual(self.starting_balance_front[Id] / (self.getFrontBalance(Id) + w(spent) + pun), 1)
+
+    def test_auto_close_inactive_account(self):
+        Id = 6
+        f: Client = self.fronts[Id]
+        b: Server = self.back
+
+        w = W3cls.ethToWei
+
+        content = b.content
+
+        def create_buy(item, c=None):
+            if c is None:
+                c = f.st.createNewChannel(w(25))
+            price = content[item]["price"]
+            r = f.buy(price, channel_number=c)
+            self.assertTrue(b.approveBuy(item, self.receipt_convert(r)))
+            return c, price
+
+        c, spent = create_buy("monalisa")
+        b.st.INACTIVITY_PERIOD = 2
+
+        print(f"Waiting INACTIVITY_PERIOD = {b.st.INACTIVITY_PERIOD}")
+        time.sleep(b.st.INACTIVITY_PERIOD)
+        b.performMaintenanceUserAccounts()
+        self.assertAlmostEqual(self.starting_balance_back / (self.getBackBalance() - w(spent)), 1)
+        self.assertAlmostEqual(self.starting_balance_front[Id] / (self.getFrontBalance(Id) + w(spent)), 1)
 
 
 if __name__ == '__main__':
